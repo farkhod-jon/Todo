794ea0b8eeca40a818ec2e710a6fe9e0ba91cd52
farkhod-jon <farhodmahmudjonov01@gmail.com>
2023-01-29 11:21:56 +0500

    done

--- script.js
+++ script.js
@@ -5,15 +5,35 @@ const myTodos = document.querySelector('.todos');
 btn.onclick = (() => {
     let val = input.value;
     Todos(val);
-
+    if(val === '' && val){
+        input.style.borderColor = 'red';
+    }
     input.value = '';
+
 })
 
 
 const Todos = ((val) => {
     const myTodo = document.createElement('div')
-    myTodo.innerHTML = val;    
+    const removeButton = document.createElement('button') 
+    myTodo.innerHTML = val; 
+    myTodo.style.display = 'block';
+    myTodo.style.direction = 'row';
     myTodos.append(myTodo);
+    myTodos.append(removeButton);
+    myTodos.style.display = 'flex';
+    myTodos.style.direction = 'column';
+    removeButton.innerHTML = 'Delete';
+    removeButton.style.backgroundColor = 'red';
+    removeButton.style.color = 'white';
+    removeButton.style.border = 'red';
+;
+
+    removeButton.onclick = (() => {
+        myTodo.remove('');
+        removeButton.remove('');
+    })
+    
 })
 
 
